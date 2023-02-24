0fbe3610882944e2fb222e57f5ad9fc22b48a1a4
nurmersb <nils.urmersbach@capgemini.com>
2023-02-24 09:36:12 +0100

    Initial commit: consuming a vue component from vue-cli-comp-lib

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div>This is the consuming project</div>
+    <hr>
+    <div>
+      Straight from the library 
+      <MyButton />
+    </div>
+    <hr>
+    <div>More of the consuming project</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import MyButton from 'vue-cli-comp-lib';
+
+
+@Component
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
